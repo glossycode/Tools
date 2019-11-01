4b7992bc5be5d8c594d932bf65919ce974e884c3
Olivier Champion <glossycode@me.com>
2019-11-01 11:37:45 +0100

    'improve'

--- DeleteBinObj/Program.cs
+++ DeleteBinObj/Program.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DeleteBO
+namespace DeleteBinObj
 {
     class Program
     {
         static log4net.ILog _logger;
+        static bool _hadException = false;
+        static int _Counter = 0;
+
         static void Main(string[] args)
         {
             Logger.Setup(false);
@@ -17,6 +20,7 @@ namespace DeleteBO
             _logger = log4net.LogManager.GetLogger(nameof(Program));
             _logger.Info("Deleting bin and obj folders. sure ? [Y]");
             ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine();
 
             if (key.Key == ConsoleKey.Y)
             {
@@ -27,22 +31,35 @@ namespace DeleteBO
                 }
                 catch (System.Exception ex)
                 {
+                    _hadException = true;
                     Console.WriteLine(ex.Message);
                 }
             }
+
+
+
+            if (_hadException)
+            {
+                Console.WriteLine($"Had exception... press any key to terminate ");            
+            }                       
+            Console.WriteLine($"delete {_Counter} folders");
+            Console.WriteLine($"press any key to terminate ");
+            Console.ReadKey();            
         }
         private static void DeleteSubFolders(string path, List<string> folders)
         {            
             foreach (string d2 in Directory.EnumerateDirectories(path, "*", SearchOption.AllDirectories)
-                .Where(s => folders.Contains(new DirectoryInfo(s).Name)))
+                .Where(s => folders.Contains(new DirectoryInfo(s).Name) && !s.Contains("\\packages\\")))
             {
                 try
                 {
                     _logger.Info($"Deleting : {d2}");
                     Directory.Delete(d2, true);
+                    _Counter++;
                 }
                 catch (Exception ex)
                 {
+                    _hadException = true;
                     _logger.Error($"Error   : {ex}");
                 }
             }
